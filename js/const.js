38ff826bbb0c4b02b988d60887f7d36bc59bea09
Andrey Slyusar <roundrobinstudio@yandex.ru>
2022-07-04 02:03:37 +0300

    Добавлена отрисовка миниатюр - 7.15(1)

--- js/const.js
+++ js/const.js
@@ -1,11 +1,11 @@
 export const PHOTOS_COUNT = 25;
 export const MAX_COMMENTS_COUNT = 3;
+export const MIN_RANGE_RANDOM_ID_COMMENT = 1;
 export const MAX_RANGE_RANDOM_ID_COMMENT = 1000;
 export const MIN_LIKES_COUNT = 15;
 export const MAX_LIKES_COUNT = 200;
 export const MIN_AVATARS_COUNT = 1;
 export const MAX_AVATARS_COUNT = 6;
-
 export const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -14,7 +14,6 @@ export const MESSAGES = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-
 export const NAMES = [
   'Пётр',
   'Алексей',
